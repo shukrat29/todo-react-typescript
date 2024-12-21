70712978a51ee4b59cc2c8d0fdf5fef6f954c73c
Shukra Tamang <shukratamang.npl@gmail.com>
2024-12-21 15:57:30 +1100

    todo list render

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
 import { Todo } from "./model";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
@@ -9,12 +10,19 @@ const App: React.FC = () => {
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
   };
+  console.log(todos);
 
   return (
     <div className="App">
       <h1 className="heading">Todo App</h1>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
